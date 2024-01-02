3c6baaa55264295dcfcf5069a8dcd4bd276805ed
Prathima-Goutahm <prathima.puranik@yahoo.co.uk>
2024-01-02 08:46:56 +0000

    First commit

--- old_stuff/Python/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost:5432');
-   define('DB_USERNAME', 'postgres');
-   define('DB_PASSWORD', 'Simpleton99'); //modify it as per your password
-   define('DB_DATABASE', 'postgres'); //modify it as per your databse
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
